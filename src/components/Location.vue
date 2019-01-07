c5c661f80c65bca146548e2acde066f7f6c44ecb
MikeKowalsky <mkowalewski83@gmail.com>
2019-01-07 12:30:07 +0100

    implemented geolocation

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -5,6 +5,22 @@
 
     <div class="container">
 
+      <button @click="showLocation">Get location</button>
+
+      <div v-if="active" class="location">
+        <div v-if="isLoading">
+          <p>... loading ...</p>
+          <p>... please wait ...</p>
+        </div>
+
+        <div v-else>
+          <p>Your coordinates</p>
+          <p>Lat: {{coordinates.lat}}</p>
+          <p>Lng: {{coordinates.lng}}</p>
+
+        </div>
+      </div>
+
     </div>
 
   </div>
@@ -12,7 +28,30 @@
 
 <script>
 export default {
-  name: "location"
+  name: "location",
+  data() {
+    return {
+      coordinates: {},
+      isLoading: true,
+      active: false
+    };
+  },
+  methods: {
+    showLocation() {
+      this.active = true;
+      // const options = {
+      //   enableHighAccuracy: true, //defaults to false
+      //   timeout: Infinity, //defaults to Infinity
+      //   maximumAge: 1 //defaults to 0
+      // };
+      this.$getLocation().then(coordinates => {
+        // this.$getLocation(options).then(coordinates => {
+        this.coordinates = coordinates;
+        this.isLoading = false;
+        console.log(this.coordinates);
+      });
+    }
+  }
 };
 </script>
 
@@ -21,5 +60,26 @@ export default {
   margin-top: 10px;
   height: calc(100vh - 160px);
   overflow: scroll;
+}
+.location {
+  margin-top: 30px;
+}
+button {
+  width: 75%;
+  height: 40px;
+  border: 1px solid black;
+  border-radius: 10px;
+  opacity: 0.9;
+
+  font-size: 20px;
+  margin-top: 50px;
+}
+button:focus {
+  outline: none;
+}
+p {
+  /* color: black; */
+  font-size: 20px;
+  font-weight: bold;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGeolocation from "vue-browser-geolocation";
+
+Vue.use(VueGeolocation);
 
 Vue.config.productionTip = false;
 
 import secretData from "./config";
-console.log(secretData);
+// console.log(secretData);
 
 new Vue({
   router,
